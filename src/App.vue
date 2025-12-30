4bd06e707da2eb1fdc705976432a7e9787e589b4
airwave <1345231099@qq.com>
2025-12-30 10:34:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import EngineComponent from './omni-engine/components/EngineComponent.vue'
+</script>
+
+<template>
+  <EngineComponent />
+</template>
+
+<style scoped>
+
+</style>
